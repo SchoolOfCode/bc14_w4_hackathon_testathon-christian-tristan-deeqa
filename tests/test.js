44b024c414236a1ea12b3538488e000c0ca46c5e
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-04-06 14:30:48 +0100

    calendar check

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-// User goes to the website
-// User clicks on the Task input box
-// User a task in the input box
-// User clicks on the completion date box
+// User goes to the website ✅
+// User clicks on the Task input box ✅
+// User puts a task in the input box ✅
+
 // User clicks on the date
 // User clicks on the "Create!" button
 
@@ -11,9 +11,32 @@ test ('Navigate to website', async ({ page }) => {
     await page.goto('http://localhost:3000');
 });
 
+//Tests 1-3
 test ('Clicking on the text box and filling it with the text "test"', async ({ page }) => {
     await page.goto('http://localhost:3000');
     const input = page.getByLabel(`Task`)
     await input.fill ('test'); 
     await expect (input).toHaveValue ('test');
+});
+
+
+// User clicks on the completion date box
+
+test ('Clicking on the date box"', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    const input = page.getByLabel(`Task`)
+    await input.fill ('test'); 
+    await expect (input).toHaveValue ('test');
+    const inputDate = page.getByLabel('Completion Date');
+    await inputDate.click();
+   });
+
+   // Use fills in the date
+test ('Filling in the date box"', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    const inputDate = page.getByLabel('Completion Date');
+    // await inputDate.click();
+    await expect (inputDate).toBeVisible();
+    await inputDate.fill ('2021-12-31');
+    await expect (inputDate).toHaveValue ('2021-12-31');
 });
